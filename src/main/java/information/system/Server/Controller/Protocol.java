6049818fda3b2a8e4744e50eb459ff72acbcf82b
Alex <plachkovskyy@gmail.com>
2019-06-23 00:02:22 +0300

    Alex changes: - Server

--- src/main/java/information/system/Server/Controller/Protocol.java
+++ src/main/java/information/system/Server/Controller/Protocol.java
@@ -5,19 +5,19 @@ package information.system.Server.Controller;
  * @author Alexands Plachkovskyy
  */
 public interface Protocol {
-    /*static final */String ADD_DISH              = "add_dish";
-    /*static final */String ADD_DISH_CATEGORY     = "add_dish_category";
-    /*static final */String SIGN_UP               = "sign_up";
-    /*static final */String DELETE_DISH           = "delete_dish";
-    /*static final */String DELETE_DISH_CATEGORY  = "delete_dish_caetgory";
-    /*static final */String DELETE_USER           = "delete_user";
-    /*static final */String EDIT_DISH             = "edit_dish";
-    /*static final */String EDIT_DISH_CATEGORY    = "edit_dish_category";
-    /*static final */String EDIT_USER             = "edit_user";
-    /*static final */String GET_INFORMATION       = "get_information";
-    /*static final */String END_OF_SESSION        = "end_of_session";
-    /*static final */String SIGN_IN               = "sign_in";
-    /*static final */String FIND_DISH             = "find_dish";
-    /*static final */String SORT_BY_PRICE         = "sort_by_price";
-    /*static final */String SORT_BY_DISH_CATEGORY = "sort_by_dish_category";
+    String ADD_DISH              = "add_dish";
+    String ADD_DISH_CATEGORY     = "add_dish_category";
+    String SIGN_UP               = "sign_up";
+    String DELETE_DISH           = "delete_dish";
+    String DELETE_DISH_CATEGORY  = "delete_dish_caetgory";
+    String DELETE_USER           = "delete_user";
+    String EDIT_DISH             = "edit_dish";
+    String EDIT_DISH_CATEGORY    = "edit_dish_category";
+    String EDIT_USER             = "edit_user";
+    String GET_INFORMATION       = "get_information";
+    String END_OF_SESSION        = "end_of_session";
+    String SIGN_IN               = "sign_in";
+    String FIND_DISH             = "find_dish";
+    String SORT_BY_PRICE         = "sort_by_price";
+    String SORT_BY_DISH_CATEGORY = "sort_by_dish_category";
 }
