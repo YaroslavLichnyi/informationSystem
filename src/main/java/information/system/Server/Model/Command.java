7489e0f3959639fa25118f0cdca6188696d64eb2
Yaroslav Lichnyi <44211382+YaroslavLichnyi@users.noreply.github.com>
2019-06-28 11:54:02 +0300

    Updated by Yaroslav

--- src/main/java/information/system/Server/Model/Command.java
+++ src/main/java/information/system/Server/Model/Command.java
@@ -1,9 +1,9 @@
 package information.system.Server.Model;
 
 public interface Command {
-    static final String SERVER_FILE_RESTAURANT = ".\\server_data\\restaurant.xml";
-    static final String SERVER_FILE_ADMINS     = ".\\server_data\\users.xml";
-    static final String SIGN_IN                = "sign_in";
-    static final String PRICE                  = "price";
-    static final String DISH_CATEGORY          = "dish category";
+    String SERVER_FILE_RESTAURANT = ".\\server_data\\restaurant.xml";
+    String SERVER_FILE_ADMINS     = ".\\server_data\\users.xml";
+    String SIGN_IN                = "sign_in";
+    String PRICE                  = "price";
+    String DISH_CATEGORY          = "dish category";
 }
