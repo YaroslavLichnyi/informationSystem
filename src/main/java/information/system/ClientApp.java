350e522f11b334eca999ee7a41648d29981f4e7a
Yaroslav Lichnyi <44211382+YaroslavLichnyi@users.noreply.github.com>
2019-06-29 20:56:37 +0300

    Updated by Yaroslav

--- src/main/java/information/system/ClientApp.java
+++ src/main/java/information/system/ClientApp.java
@@ -3,12 +3,8 @@ package information.system;
 import information.system.Client.Controller.Client;
 import information.system.Client.View.ChangePortForm;
 
-/**
- * Class for lunching Client
- */
-public class ClientApp
-{
-    public static void main( String[] args ) {
+public class ClientApp {
+    public static void main(String[] args) {
         new ChangePortForm(new Client());
     }
 }
--- src/main/java/information/system/ServerApp.java
+++ src/main/java/information/system/ServerApp.java
@@ -1,8 +1,5 @@
 package information.system;
 
-/**
- * Class for lunching Server console version
- */
 public class ServerApp {
     public static void main(String[] args) {
 
