ae68b0d07c14179ca41188ad4569db6e5d2dffe4
Alex <plachkovskyy@gmail.com>
2019-07-18 18:51:58 +0300

    Alex changes: - Server - the last commit before defence.

--- client-run.bat
+++ client-run.bat
@@ -1,3 +1,3 @@
 #java -classpath target/information.system-1.0-SNAPSHOT-shaded.jar information.system.ClientApp
-java -classpath target/client-runnable.jar information.system.ClientApp
+java -classpath target/client-runnable-jar-with-dependencies.jar information.system.ClientApp
 pause
